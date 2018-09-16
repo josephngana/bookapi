8102fde95bf33ddf349a61f72d34b4fb949255a2
caniksea <caniksea@yahoo.co.nz>
2018-09-16 14:50:46 +0200

    Made BaseBook abstract instead of a case class; to fix the compilation error.

--- app/domain/Book.scala
+++ app/domain/Book.scala
@@ -5,13 +5,13 @@ import java.time.LocalTime
 import play.api.libs.json.Json
 
 case class Book(
-                 override val id: String,
-                 override val title: String,
+                 id: String,
+                 title: String,
                  override val description: String = "",
                  override val story: String = "",
                  isbn: String,
                  datePublished: LocalTime,
-               ) extends BookBase(id, title, description, story) {}
+               ) extends BookBase {}
 
 object Book {
   implicit val bookFormat = Json.format[Book]
--- app/domain/BookBase.scala
+++ app/domain/BookBase.scala
@@ -2,10 +2,10 @@ package domain
 
 import java.time.LocalTime
 
-import play.api.libs.json.Json
-
-case class BookBase(id: String, title: String, description: String = "", story: String = "", dateCreated: LocalTime = LocalTime.now) {}
-
-object BookBase {
-  implicit val bookBaseFormat = Json.format[BookBase]
+abstract class BookBase() {
+  def id: String
+  def title: String
+  def description: String = ""
+  def story: String = ""
+  def dateCreated: LocalTime = LocalTime.now
 }
